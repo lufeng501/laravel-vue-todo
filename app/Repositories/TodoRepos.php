486ef19669831b1000ba75667e463ec48b8cc71e
lufeng501 <lufeng501@126.com>
2017-08-15 16:40:48 +0800

    mint-ui重构

--- app/Repositories/TodoRepos.php
+++ app/Repositories/TodoRepos.php
@@ -11,6 +11,7 @@ namespace App\Repositories;
 
 
 use App\Models\TodoLists;
+use Carbon\Carbon;
 
 class TodoRepos
 {
@@ -21,30 +22,41 @@ class TodoRepos
      */
     public function getTodoListsDetails($conditions = [])
     {
-        $todoListsModel = TodoLists::select(['id','title','status','created_at','updated_at']);
-        if (!empty($conditions['item_type']) && in_array($conditions['item_type'],[2,3])) {
+        $todoListsModel = TodoLists::select(['id', 'title', 'status', 'created_at', 'updated_at']);
+        if (!empty($conditions['item_type']) && in_array($conditions['item_type'], [2, 3])) {
             // 未完成
             if ($conditions['item_type'] == 2) {
-                $todoListsModel = $todoListsModel->where('status',0);
+                $todoListsModel = $todoListsModel->where('status', 0);
             }
             // 已完成
             if ($conditions['item_type'] == 3) {
-                $todoListsModel = $todoListsModel->where('status',1);
+                $todoListsModel = $todoListsModel->where('status', 1);
             }
         }
+
+        // 时间区间搜索
+        if (!empty($conditions['start_date'])) {
+            $todoListsModel = $todoListsModel->where('created_at', ">=", $conditions['start_date']);
+        }
+
+        if (!empty($conditions['end_date'])) {
+            $endData = Carbon::createFromTimestamp(strtotime($conditions['end_date']))->addDay()->toDateString();
+            $todoListsModel = $todoListsModel->where('created_at', "<=", $endData);
+        }
+
         // 判断排序类型
-        if (!empty($conditions['order_type']) && in_array($conditions['order_type'],[1,2,3])) {
+        if (!empty($conditions['order_type']) && in_array($conditions['order_type'], [1, 2, 3])) {
             // 名称排序
             if ($conditions['order_type'] == 1) {
-                $todoListsModel = $todoListsModel->orderBy('title','asc');
+                $todoListsModel = $todoListsModel->orderBy('title', 'asc');
             }
             // 创建时间排序
             if ($conditions['order_type'] == 2) {
-                $todoListsModel = $todoListsModel->orderBy('created_at','desc');
+                $todoListsModel = $todoListsModel->orderBy('created_at', 'desc');
             }
             // 更新时间排序
             if ($conditions['order_type'] == 3) {
-                $todoListsModel = $todoListsModel->orderBy('updated_at','desc');
+                $todoListsModel = $todoListsModel->orderBy('updated_at', 'desc');
             }
         }
         $lists = $todoListsModel->get();
@@ -57,7 +69,7 @@ class TodoRepos
         // 变量待办事项，获得详细的待办描述
         if (!empty($lists) && is_array($lists)) {
             foreach ($lists as $key => &$value) {
-                $relatedData = TodoLists::findOrFail($value['id'])->details()->select(['id','title','status','list_id'])->get();
+                $relatedData = TodoLists::findOrFail($value['id'])->details()->select(['id', 'title', 'status', 'list_id'])->get();
                 if (!empty($relatedData)) {
                     $relatedData = $relatedData->toArray();
                 } else {
@@ -79,14 +91,14 @@ class TodoRepos
     public function getTodoDetailsById($id)
     {
         $details = [];
-        $todoModel = TodoLists::select(['id','title','status','created_at','updated_at'])->where('id',$id)->first();
+        $todoModel = TodoLists::select(['id', 'title', 'status', 'created_at', 'updated_at'])->where('id', $id)->first();
         if (empty($todoModel)) {
             return $details;
         }
         $details = $todoModel->toArray();
         // 获取关联的待办描述
         if (!empty($details)) {
-            $relatedData = TodoLists::findOrFail($details['id'])->details()->select(['id','title','status','list_id'])->get();
+            $relatedData = TodoLists::findOrFail($details['id'])->details()->select(['id', 'title', 'status', 'list_id'])->get();
             if (!empty($relatedData)) {
                 $relatedData = $relatedData->toArray();
             } else {
